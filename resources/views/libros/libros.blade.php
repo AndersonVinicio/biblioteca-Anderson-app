d0182e387df01568983e8a4360b2fb3c3162e0a7
Anderson <andyvini.developer@gmail.com>
2023-09-10 18:22:31 +0200

    se ha implementado un boton en la tabla de libros el cual nos permitira realizar la reserva del libro en el caso que no este disponible estara desabilitado

--- resources/views/libros/libros.blade.php
+++ resources/views/libros/libros.blade.php
@@ -1,47 +1,51 @@
 @extends('app')
 @section('title', 'LIBROS')
 @section('content')
-    {{-- @livewire('tabla-libros') --}}
-    <nav>
-        <ul>
-            <li><a href="/formulario-add-book">AÑADIR LIBRO</a></li>
-        </ul>
-    </nav>
+{{-- @livewire('tabla-libros') --}}
+<nav>
+    <ul>
+        <li><a href="/formulario-add-book">AÑADIR LIBRO</a></li>
+    </ul>
+</nav>
 
 
 
-    <table>
-        <form action={{ route('searchBook') }} method="post">
-            @csrf
-            <label for="inputBuscarLibro">Buscar:</label>
-            <input type="text" name="inputBuscarLibro" id="inputBuscarLibro" placeholder="itroduce tiulo o isbn">
-            <button type="submit">BUSCAR</button>
-            <a class="botonesTablaLibros" href="/libros">Borra busqueda</a>
-        </form>
-        @if (!empty($libros))
-        
-           <tr>
-            <th>TITULO LIBRO</th>
-            <th>ISBN</th>
-        </tr>
-        @foreach ($libros as $libro)
-            <tr>
-                <td>{{ $libro->titulo }}</td>
-                <td>{{ $libro->isbn }}</td>
-                <td>
-                    <a class="botonesTablaLibros eliminar"
-                        href="http://127.0.0.1:8000/delete-libro/{{ $libro->id }}">ELIMINAR</a>
-                    <a class="botonesTablaLibros" href="http://127.0.0.1:8000/edit-libro/{{ $libro->id }}">EDITAR</a>
-                    <a class="botonesTablaLibros" href="http://127.0.0.1:8000/detalles-libro/{{ $libro->id }}">MAS
-                        DETALLES</a>
-                    <a class="botonesTablaLibros" href="#">Prestar</a>
-                </td>
-            </tr>
-        @endforeach
-        @else
-        <h2 style="color: red">NO SE ENCONTRO NINGUN LIBRO</h2>
-           
-        @endif
-    </table>
+<table>
+    <form action={{ route('searchBook') }} method="post">
+        @csrf
+        <label for="inputBuscarLibro">Buscar:</label>
+        <input type="text" name="inputBuscarLibro" id="inputBuscarLibro" placeholder="itroduce tiulo o isbn">
+        <button type="submit">BUSCAR</button>
+        <a class="botonesTablaLibros" href="/libros">Borra busqueda</a>
+    </form>
+    @if (!empty($libros))
+
+    <tr>
+        <th>TITULO LIBRO</th>
+        <th>ISBN</th>
+    </tr>
+    @foreach ($libros as $libro)
+    <tr>
+        <td>{{ $libro->titulo }}</td>
+        <td>{{ $libro->isbn }}</td>
+        <td>
+            <a class="botonesTablaLibros eliminar"
+                href="http://127.0.0.1:8000/delete-libro/{{ $libro->id }}">ELIMINAR</a>
+            <a class="botonesTablaLibros" href="http://127.0.0.1:8000/edit-libro/{{ $libro->id }}">EDITAR</a>
+            <a class="botonesTablaLibros" href="http://127.0.0.1:8000/detalles-libro/{{ $libro->id }}">MAS
+                DETALLES</a>
+            @if ($libro->disponible === 1)
+                <a class="botonesTablaLibros" href="/formulario-add-prestamo">Prestar</a>
+            @else
+            <a class="disabled-link" href="#">No disponible</a>
+            @endif
+
+        </td>
+    </tr>
+    @endforeach
+    @else
+    <h2 style="color: red">NO SE ENCONTRO NINGUN LIBRO</h2>
+
+    @endif
+</table>
 @endsection
-
